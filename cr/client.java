1e3d97262b6afa2db43fc783244a6fa46391b178
Ashley M <ashleyemaurer@gmail.com>
2024-04-16 20:18:05 -0400

    added user input commands and server responses

---
+++ cr/client.java
@@ -0,0 +1,88 @@
+package cr;
+import java.io.*;
+import java.net.*;
+
+public class client {
+    private Socket socket = null;
+    private DataInputStream in = null;
+    private DataOutputStream out = null;
+    private String inFromServer = "";
+    private String outToServer = "";
+    private BufferedReader reader = null;
+
+    public client(String hostname, int port) {
+        try {
+            // Setting up connection
+            System.out.println("Attempting to connect to " + hostname + " on port " + port);
+            socket = new Socket(hostname, port);
+            System.out.println("Successfully connected to " + hostname + " on port " + port);
+
+            // Initializing input/output streams
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            reader = new BufferedReader(new InputStreamReader(System.in));
+
+            //Server sends "Hello!" to ensure connection is established before client can send a message
+            inFromServer = in.readUTF();
+            System.out.println("Received from server: " + inFromServer);
+            System.out.println("Enter events to schedule. Enter 'send' to receive a schedule. Enter 'bye' to exit.");
+            boolean receiveFile = false;
+
+            while (!outToServer.equals("bye")) {
+                while(!receiveFile) {
+                    try {
+                        System.out.print("Send server event: ");
+                        outToServer = reader.readLine();
+                        out.writeUTF(outToServer);
+                        out.flush();
+    
+                        inFromServer = in.readUTF();
+                        System.out.println("Received server response: " + inFromServer);
+                        if (inFromServer.equals("Sending file")) {
+                            receiveFile = true;
+                        }
+                    } catch (Exception e) {
+                        System.out.println(e);
+                    }
+                }
+                try {
+                    //Write schedule file received from server to cr directory
+                    if (receiveFile) {
+                        byte[] fileBytes = new byte[1024];
+                        FileOutputStream fileOut = new FileOutputStream("./cr/" + "schedule.txt");
+                        BufferedOutputStream fileWriter = new BufferedOutputStream(fileOut);
+                        int totalBytes = in.read(fileBytes, 0, fileBytes.length);
+                        fileWriter.write(fileBytes, 0, totalBytes);
+
+                        fileWriter.close();
+                        fileOut.close();
+                        receiveFile = false;
+                    }
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+
+            // Close connection
+            System.out.println("Exiting");
+            out.close();
+            in.close();
+            socket.close();
+        } catch (Exception e) {
+            System.out.println(e);
+            System.exit(-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            String hostname = args[0];
+            int port = Integer.valueOf(args[1]);
+            //Call client function with given port and hostname arguments to initialize client/client socket
+            client client = new client(hostname, port);
+        } catch (Exception e) {
+            System.out.println(e);
+            System.exit(-1);
+        }
+    }
+}
---
+++ sr/server.java
@@ -0,0 +1,94 @@
+package sr;
+import java.io.*;
+import java.lang.*;
+import java.net.*;
+import java.util.*;
+
+public class server {
+    private Socket socket = null;
+    private ServerSocket serverSocket = null;
+    private DataInputStream in = null;
+    private DataOutputStream out = null;
+    private String clientInput = "";
+
+    public server(int port) {
+        try {
+            // Setting up connection
+            serverSocket = new ServerSocket(port);
+            System.out.println("Created server on port " + port);
+
+            socket = serverSocket.accept();
+            System.out.println("Got connection address from " + socket.getInetAddress().toString() + " on port " + port);
+
+            // Initializing input/output streams
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            out.writeUTF("Hello!");
+            
+            boolean requestedFile = false;
+            String fileName = "";
+            ArrayList<String> eventNames = new ArrayList<String>();
+            ArrayList<Integer> eventTimes = new ArrayList<Integer>();
+            
+
+
+            while (!clientInput.equals("bye")) {
+                while(!requestedFile){
+                    try {
+                        clientInput = in.readUTF();
+                        if (clientInput.equals("send")) {
+                            System.out.println("Client said:" + clientInput);
+                            out.writeUTF("send request received");
+                            requestedFile = true;
+                        } else {
+                            System.out.println("Client event: " + clientInput);
+                            out.writeUTF("event received");
+                        }
+                    } catch (Exception e) {
+                        System.out.println(e);
+                    }
+                }
+                // Send desired file to client
+                System.out.println("Sending schedule file1"); //TODO
+                if(requestedFile) {
+                    System.out.println("Sending schedule file");
+                    out.writeUTF("Sending file");
+                    File sFile = new File("./sr/schedule.txt");
+                    byte[] fileBytes = new byte[(int) sFile.length()];
+                    FileInputStream fileIn = new FileInputStream(sFile);
+                    BufferedInputStream fileReader = new BufferedInputStream(fileIn);
+                    fileReader.read(fileBytes, 0, fileBytes.length);
+
+                    out.write(fileBytes, 0, fileBytes.length);
+                    fileIn.close();
+                    fileReader.close();
+                    requestedFile = false;
+                }
+                
+
+            }
+
+            // Close connection
+            System.out.println("Received disconnect signal from client address " + socket.getInetAddress().toString() + " on port " + port);
+            System.out.println("Exiting");
+            out.writeUTF("Disconnected");
+            in.close();
+            out.close();
+            socket.close();
+            serverSocket.close();
+        } catch (Exception e) {
+            System.out.println("Error listening on port " + port);
+            System.exit(-1);
+        }
+    }
+    public static void main(String[] args) {
+        try {
+            int port = Integer.valueOf(args[0]);
+            //Call server function with given port argument to initialize server/server sockets
+            server s = new server(port);
+        } catch (Exception e) {
+            System.out.println("Failed to capture command line arguments");
+            System.exit(-1);
+        }
+    }
+}
